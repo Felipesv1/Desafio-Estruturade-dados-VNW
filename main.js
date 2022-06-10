48129248bb007a02b22a161cf1de69adc4f918f1
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-06-09 23:11:19 -0300

    Estrutura de dados

---
+++ main.js
@@ -0,0 +1,34 @@
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+
+
+let nome = "Felipe";
+console.log(nome)
+
+let numero = 7; 
+console.log(numero)
+
+let anoAtual = 2022;
+console.log(anoAtual - numero)
+
+console.log(anoAtual + numero)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true;
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
+
